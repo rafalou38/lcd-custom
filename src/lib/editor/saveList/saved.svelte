0c1842866b4396a48306961d76fc0580333a162d
Rafael <rafael.martinezcalvo@orange.fr>
2021-12-16 19:15:27 +0100

    remove unnecesary components

--- src/lib/editor/saveList/saved.svelte
+++ src/lib/editor/saveList/saved.svelte
@@ -1,5 +1,17 @@
-<script>
-	import SaveList from './SaveList.svelte';
+<script lang="ts">
+	import Character from '$lib/components/character.svelte';
+	import {
+		curentCharacter,
+		defaultCharacter,
+		savedCharacters
+	} from '$lib/editor/stores/characters';
 </script>
 
-<SaveList />
+<h2 class="w-full mb-2 text-gray-500 font-semibold">Saved</h2>
+<ul class="flex flex-wrap gap-2">
+	{#each $savedCharacters as character}
+		<Character {character} />
+	{:else}
+		No characters saved.
+	{/each}
+</ul>
