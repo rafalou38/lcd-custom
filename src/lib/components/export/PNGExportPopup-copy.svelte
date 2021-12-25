bfa43a37248e6cf908a084f7cd50ed6f75383946
Rafael <rafael.martinezcalvo@orange.fr>
2021-12-25 17:06:30 +0100

    add png export

--- src/lib/components/export/PNGExportPopup-copy.svelte
+++ src/lib/components/export/PNGExportPopup-copy.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import Popup from '$lib/components/popup.svelte';
-	import type { Character } from '$lib/types/character';
+
+	import { toPNG } from '$lib/utils/grid';
 	import { createEventDispatcher } from 'svelte/internal';
+	import Character from '../character.svelte';
 	import { currentExport } from './store';
+	let imageUrl = '';
+
+	$: imageUrl = toPNG($currentExport.character.grid);
 </script>
 
 <Popup on:close={() => currentExport.set(null)}>
-	<div class="flex flex-col p-4 h-full" />
+	<div class="relative flex justify-center items-center p-4 h-full">
+		<a href={imageUrl} target="_blank" class="w-1/2"
+			><img src={imageUrl} alt="" class="w-full crisp" /></a
+		>
+	</div>
 </Popup>
