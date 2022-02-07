d1dcc991ce174203d232cf3d5db3429f8822a328
Rafael <rafael.martinezcalvo@orange.fr>
2022-02-07 13:59:29 +0100

    add homepage

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<div class="container mx-auto">
+	<h1 class="my-4 text-4xl font-bold">Find and create lcd characters.</h1>
+	<div class="ml-2">
+		<h2 class="my-2 text-2xl font-bold">Create</h2>
+		<div class="ml-2">
+			<p>Use the editor to Browse and create your own characters</p>
+			<a href="/editor" class="flex w-max rounded bg-indigo-600 px-4 py-2 text-white">Open Editor</a
+			>
+		</div>
+		<h2 class="my-2 text-2xl font-bold">Browse</h2>
+		<div class="ml-2">
+			<p>Search for characters made by the community, or search inspiration.</p>
+			<a href="/editor" class="flex w-max rounded bg-indigo-600 px-4 py-2 text-white"
+				>Browse characters</a
+			>
+		</div>
+	</div>
+</div>
